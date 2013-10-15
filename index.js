8056bd6c51500b0fa45231a3b7a3fafae5766d07
Lorenzo García Rivera <lorenzogrv@gmail.com>
2013-10-15 17:26:13 +0200

    added iai-is as package dependency and git submodule, import code from conservas currican, add unit tests

---
+++ index.js
@@ -0,0 +1,140 @@
+var assert = require( 'assert' )
+  , isFn = require( 'iai-is' )( 'Function' )
+;
+
+/**
+ * # test utils #
+ * @function test: returns a set of pre-defined test cases
+ *   @param cases [String]: must be within Object.keys( test.cases );
+ */
+
+var exports = module.exports = test;
+
+exports.version = "1";
+exports.stability = 1;
+
+function test( cases ){
+  if( !~Object.keys( test.cases ).indexOf(cases) ){
+    throw Error( "["+cases+"] is not within test.cases");
+  }
+  return test.cases[ cases ];
+}
+
+/**
+ * ## Generic test cases ##
+ *
+ * A collection of generic test cases, meant to be used on unit testing.
+ *
+ */
+
+test.cases = {
+  "primitive string empty": "",
+  "primitive string whitespace characters": "        ",
+  "primitive string tab characters": "\t\t",
+  "primitive string alphanumeric characters": "abcdefghijklm1234567890",
+  "primitive string non-numeric characters": "xabcdefx",
+  "primitive string numeric with preceding non-numeric characters": "bcfed5.2",
+  "primitive string numeric with trailling non-numeric characters": "7.2acdgs",
+
+  "primitive string integer decimal number zero": "0",
+  "primitive string integer decimal negative number": "-10",
+  "primitive string integer decimal positive number": "5",
+  "primitive string integer octal number": "040",
+  "primitive string integer hexadecimal number": "0xFF",
+  "primitive string floating-point negative number": "-1.6",
+  "primitive string floating-point positive number": "4.536",
+  "primitive string floating-comma negative number": "-4,536",
+  "primitive string floating-comma positive number": "4,536",
+  "primitive string exponential notation number": "123e-2",
+
+  "primitive integer decimal number zero": 0,
+  "primitive integer decimal negative number": -16,
+  "primitive integer decimal positive number": 32,
+  "primitive integer octal number": 0144,
+  "primitive integer hexadecimal number": 0xFFF,
+
+  "primitive floating-point negative number": -2.6,
+  "primitive floating-point negative number": 3.1415,
+  "primitive exponential notation number": 8e5,
+
+  "primitive boolean true": true,
+  "primitive string true": "true",
+  "primitive boolean false": false,
+  "primitive string false": "false",
+
+  "primitive infinity": Infinity,
+  "positive infinity": Number.POSITIVE_INFINITY,
+  "negative infinity": Number.NEGATIVE_INFINITY,
+
+  "primitive undefined": undefined,
+  "primitive null": null,
+  "primitive NaN": NaN,
+
+  "primitive array object": [ 1, 2, 3, 4 ],
+  "primitive regexp object": /.*/,
+  "primitive object empty": {},
+
+  "object date": new Date(2009,1,1),
+  "object empty": new Object(),
+
+  "anonymous function": function(){},
+  "named function": function name(){}
+};
+
+/**
+ * ## Object property testers ##
+ *
+ * @function defined: Asserts `o` has a property named `pname`.
+ *   @param o [Any]: The object to be tested
+ *   @param pname [String]: The property name
+ *   @returns: the property descriptor
+ *
+ * see [Object.getOwnPropertyDescriptor reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyDescriptor)
+ *
+ */
+
+test.defined = function( o, pname ){
+  var descriptor = Object.getOwnPropertyDescriptor( o, pname );
+  assert( descriptor, o + "#" + pname + " is not defined" );
+  return descriptor;
+};
+
+/**
+ * @function methods: Asserts `o` has defined methods named by arg 2 and onwards.
+ *   @param o [Any]: The object to be tested
+ *   @param name1 [String]: A method name
+ *   @param name2 [String]: A method name
+ *   @param nameN [String]: A method name
+ *
+ */
+test.methods = function( o ){
+  Array.prototype.slice.call( arguments, 1 )
+  .forEach(function(value){
+    test.defined( o, value );
+    assert( isFn( o[value] ), o+"#"+value+" is not a function" );
+  })
+}
+
+/**
+ * @function chainableApi: Asserts api implements methods, and methods return api
+ *   @param api [Object]: the object to be tested
+ *   @param methods [Hash]: keys are method names, values arrays with valid arguments
+ *
+ * Ex:
+ *     test.chainableApi( foo, {
+ *       "bar": [ "each", "item", "is", "an", "argument" ],
+ *       "baz": [ "arg1", "arg2", "..." ]
+ *     })
+ *
+ * This snipnet asserts:
+ *   - foo implements the properties "bar" and "baz" as functions.
+ *   - bar and baz, when applied foo and its argument array, return foo.
+ */
+test.chainableApi = function( api, methods ){
+  for( var name in methods ){
+    assert( isFn( api[name] ), api+'#'+name+' is not a function' );
+    assert.deepEqual( api[ name ].apply( api, methods[name] ), api,
+                      api+'#'+name+' does not return current context'
+    );
+  }
+};
---
+++ test/4-chainableApi.js
@@ -0,0 +1,67 @@
+describe( "#chainableApi", function(){
+  it( "should be a function", function(){
+    assert.isFunction( test.chainableApi );
+  })
+
+  var interface = {
+    // interface used for all tests
+    "foo": [ "arg1" ],
+    "bar": [ null, 134 ],
+    "baz": [ 1, 2, 3, "abcd" ]
+  };
+
+  it( "should execute methods applying arguments", function(){
+    test.chainableApi( {
+      foo: function( a ){
+        assert.equal( a, "arg1", "foo argument" )
+        return this;
+      },
+      bar: function( a1, a2 ){
+        assert.isNull( a1, "foo argument 1" )
+        assert.equal( a2, 134, "foo argument 2" )
+        return this;
+      },
+      baz: function( a1, a2, a3, a4 ){
+        assert.equal( a1, 1, "baz argument 1" )
+        assert.equal( a2, 2, "baz argument 2" )
+        assert.equal( a3, 3, "baz argument 3" )
+        assert.equal( a4, "abcd", "baz argument t" )
+        return this;
+      }
+    }, interface );
+  })
+
+  function throws( api, description, regexp ){
+    assert.throws(function(){
+      test.chainableApi( api, interface );
+    }, Error, regexp, description );
+  }
+
+  function yes(){ return this; }
+
+  function createTestCase( bad, regexp ){
+    return function(){
+      assert.doesNotThrow(function(){
+        test.chainableApi( { foo: yes, bar: yes, baz: yes }, interface );
+      }, Error, "given all good" );
+      throws( { foo: yes, bar: yes, baz: bad }, "given bad baz", regexp);
+      throws( { foo: yes, bar: bad, baz: yes }, "given bad bar", regexp );
+      throws( { foo: yes, bar: bad, baz: bad }, "given bad bar and baz", regexp );
+      throws( { foo: bad, bar: yes, baz: yes }, "given bad foo", regexp );
+      throws( { foo: bad, bar: yes, baz: bad }, "given bad foo and baz", regexp );
+      throws( { foo: bad, bar: bad, baz: yes }, "given bad foo, bar", regexp );
+      throws( { foo: bad, bar: bad, baz: bad }, "given  all bad", regexp );
+    }
+  }
+
+  it( "should assert methods to be functions", createTestCase(
+    undefined, /is not a function/
+  ))
+
+  it( "should assert methods return the current context", createTestCase(
+    function(){ }, /does not return current context/
+  ))
+  it( "should throw an error if any methods throws an error", createTestCase(
+    function(){ throw Error( "Oops"); }, /Oops/
+  ))
+})
